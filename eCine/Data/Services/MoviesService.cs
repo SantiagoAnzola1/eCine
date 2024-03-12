818999e2fdba487eb3f47d2569c90b0f0f1e073e
SantiagoAnzola <87992831+SantiagoAnzola@users.noreply.github.com>
2024-03-11 22:05:00 -0500

    get the dropdowns values

--- eCine/Data/Services/MoviesService.cs
+++ eCine/Data/Services/MoviesService.cs
@@ -1,4 +1,5 @@
 ﻿using eCine.Data.Base;
+using eCine.Data.ViewModels;
 using eCine.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,18 @@ namespace eCine.Data.Services
                 .FirstOrDefaultAsync(n=>n.Id==id);
 
             return movie;
+        }
+
+        public async Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues()
+        {
+            var response = new NewMovieDropdownsVM()
+            {
+
+                Actors = await _context.Actors.OrderBy(n => n.FullName).ToListAsync(),
+                Cinemas = await _context.Cinemas.OrderBy(n => n.Name).ToListAsync(),
+                Producers = await _context.Producers.OrderBy(n => n.FullName).ToListAsync()
+            };
+            return response;
         }
     }
 }
