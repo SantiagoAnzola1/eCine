756352c49181036c5bf132c39d3042997c02e6e3
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2024-03-17 21:44:21 -0500

    add shopping cart data
    GetShoppingCartItems && GetShoppingCartTotal

---
+++ eCine/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using eCine.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCine.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItems> ShoppingCartItems { get; set; }
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItems> GetShoppingCartItems() { 
+            return ShoppingCartItems ?? (ShoppingCartItems=_context.ShoppingCartItems.Where(n=>n.ShoppingCartId==ShoppingCartId).Include(n=>n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            return _context.ShoppingCartItems.Where(n=>n.ShoppingCartId==ShoppingCartId).Select(n=>n.Movie.Price*n.Amount).Sum();
+            
+        }
+    }
+}
