6868bab7091fdbb118ec77a02a3ffb319ed60626
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2024-03-17 22:07:56 -0500

    add ShoppingCartVM & OrdersController
    control the order view data

---
+++ eCine/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using eCine.Data.Cart;
+
+namespace eCine.Data.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
