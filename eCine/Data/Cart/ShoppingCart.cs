b6d83eb82592130ad73263c233cbee9554958701
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2024-03-17 21:54:02 -0500

    add AddItemToCart method
    control the amount of movies when are added to cart

--- eCine/Data/Cart/ShoppingCart.cs
+++ eCine/Data/Cart/ShoppingCart.cs
@@ -14,6 +14,28 @@ namespace eCine.Data.Cart
             _context = context;
         }
 
+        public void AddItemToCart(NewMovieModel movie)
+        {
+            var shoppingCartItem=_context.ShoppingCartItems.FirstOrDefault(n=>n.Movie.Id == movie.Id && n.ShoppingCartId==ShoppingCartId );
+
+            if(shoppingCartItem != null)
+            {
+                shoppingCartItem = new ShoppingCartItems()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    Amount = 1
+                };
+
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
+
         public List<ShoppingCartItems> GetShoppingCartItems() { 
             return ShoppingCartItems ?? (ShoppingCartItems=_context.ShoppingCartItems.Where(n=>n.ShoppingCartId==ShoppingCartId).Include(n=>n.Movie).ToList());
         }
