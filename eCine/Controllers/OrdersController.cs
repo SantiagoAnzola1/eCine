a71d470ca70c855d2c9a88f5f1023ff27981c9ac
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2024-03-19 14:16:49 -0500

    change the index order name to ShoppingCart

--- eCine/Controllers/OrdersController.cs
+++ eCine/Controllers/OrdersController.cs
@@ -15,7 +15,7 @@ namespace eCine.Controllers
             _moviesService = moviesService;
             _shoppingCart = shoppingCart;
         }
-        public IActionResult Index()
+        public IActionResult ShoppingCart()
         {
             var items = _shoppingCart.GetShoppingCartItems();
             _shoppingCart.ShoppingCartItems = items;
