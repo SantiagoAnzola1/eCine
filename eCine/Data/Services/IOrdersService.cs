dbc94fba0e4402e43696266ee150c364e504cc94
SantiagoAnzola1 <santiag.anzola1@gmail.com>
2024-03-26 19:53:30 -0500

    Add OrderService methods

---
+++ eCine/Data/Services/IOrdersService.cs
@@ -0,0 +1,11 @@
+﻿using eCine.Models;
+
+namespace eCine.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItems> items, string userId, string userEmailAdress);
+
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+    }
+}
--- eCine/Models/Order.cs
+++ eCine/Models/Order.cs
@@ -8,7 +8,7 @@ namespace eCine.Models
         public int Id { get; set; }
 
         public string Email { get; set; }
-        public string userId { get; set; }
+        public string UserId { get; set; }
 
         public List<OrderItem> OrderItems { get; set; }
 
