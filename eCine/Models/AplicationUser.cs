6d4a1455c862a0874c36af448357743b5bb89b34
SantiagoAnzola <87992831+SantiagoAnzola@users.noreply.github.com>
2025-06-08 12:25:12 -0500

    add AplicationUer Model for users authentication

---
+++ eCine/Models/AplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCine.Models
+{
+    public class AplicationUser:IdentityUser
+    {
+        [Display(Name ="Full Name")]
+        public string FullName { get; set; }
+    }
+}
