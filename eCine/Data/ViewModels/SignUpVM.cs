9c75e49c69deecf12499f65b36ca396775904127
SantiagoAnzola <87992831+SantiagoAnzola@users.noreply.github.com>
2025-06-14 08:15:11 -0500

    Add SignUpVM class for user registration model
    Introduces the `SignUpVM` class in the `eCine.Data.ViewModels` namespace. This class includes properties for `FullName`, `Email`, `Password`, and `ConfirmPassword`, with data annotations for validation to ensure required fields and password confirmation matching.

---
+++ eCine/Data/ViewModels/SignUpVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCine.Data.ViewModels
+{
+    public class SignUpVM
+    {
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full name is required.")]
+        public string FullName { get; set; }
+
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required (ErrorMessage = "Confirmation password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
