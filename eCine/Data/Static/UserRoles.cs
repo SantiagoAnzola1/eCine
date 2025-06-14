9a7d3cf67f03715606277bddb58b6afd05c1540e
SantiagoAnzola <87992831+SantiagoAnzola@users.noreply.github.com>
2025-06-14 08:14:51 -0500

    Add UserRoles class for role management
    Introduced a new static class `UserRoles` in the `eCine.Data.Static` namespace. This class defines two constant string fields, `Admin` and `User`, and includes a static property `Roles` that returns a list of these roles.

---
+++ eCine/Data/Static/UserRoles.cs
@@ -0,0 +1,11 @@
+﻿namespace eCine.Data.Static
+{
+
+    public static class UserRoles
+    {
+        public const string Admin = "Admin"; 
+        public const string User = "User";
+
+        public static IEnumerable<string> Roles=> new List<string> { Admin, User };
+    }
+}
