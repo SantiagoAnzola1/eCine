7a8db78ba92ebb1b9650b8817242f310b632109e
SantiagoAnzola <87992831+SantiagoAnzola@users.noreply.github.com>
2025-06-14 08:15:36 -0500

    Add UserVM class for user information management
    Introduces the `UserVM` class in the `eCine.Data.ViewModels` namespace. This class includes properties for `Id`, `FullName`, `UserName`, `Email`, and `Role`, with data annotations for validation. It also initializes a list of available roles from `UserRoles.Roles` for role selection.

---
+++ eCine/Data/ViewModels/UserVM.cs
@@ -0,0 +1,31 @@
+﻿using eCine.Data.Static;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCine.Data.ViewModels
+{
+   
+    public class UserVM
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full name is required")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Role")]
+        [Required(ErrorMessage = "Role is required")]
+        public string Role { get; set; }
+
+        public List<string> Roles { get; set; } = UserRoles.Roles.ToList(); // Available roles for selection  
+   
+    }
+}
